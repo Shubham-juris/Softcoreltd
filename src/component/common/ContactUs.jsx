f0fd0803e59bec739b19d137cc74446382d5b32f
JurisNexaIt <shubham.developerjuris@gmail.com>
2026-04-10 23:41:44 +0500

    Update ContactUs.jsx

--- src/component/common/ContactUs.jsx
+++ src/component/common/ContactUs.jsx
@@ -23,23 +23,13 @@ const ContactUs = () => {
           <h3 className="text-base md:text-lg font-semibold">
             Softcore Technologies
           </h3>
-          {/* <p className="text-gray-600 text-sm md:text-base">
+           <p className="text-gray-600 text-sm md:text-base">
             {/* 4629 3 Avenue, Edson, Alberta T7E 1C2, Canada */}
             suite 200 - 1212 - 1st se - calgary AB T2G-2H8<br></br>
             <hr></hr>
             4629 3rd Avenue Edison, Alberta, Calgary T7E 1C2, Canada
-          </p> */}
-        <p className="text-gray-600 text-sm md:text-base text-center leading-relaxed">
-  Suite 200 - 1212 - 1st SE, Calgary AB T2G 2H8
-</p>
-
-<div className="flex justify-center my-3">
-  <div className="w-16 h-px bg-gray-300"></div>
-</div>
-
-<p className="text-gray-600 text-sm md:text-base text-center leading-relaxed">
-  4629 3rd Avenue, Calgary, Alberta T7E 1C2, Canada
-</p>
+          </p> 
+     
           <a
             href="tel:+14034777967"
             className="text-orange-600 font-semibold block mt-1"
